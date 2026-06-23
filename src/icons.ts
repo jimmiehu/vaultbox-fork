8d6de160e5010cc8e917463ce5e266c8a83582bd
Steven Robbins <ste.robbins@gmail.com>
2026-06-23 08:11:31 +0100

    Simplify Vaultbox button icon

--- src/icons.ts
+++ src/icons.ts
@@ -1,12 +1,8 @@
 export const VAULTBOX_ICON_PATHS = `
-<path d="M15 49 50 37l35 12-35 13-35-13Z" fill="currentColor" opacity=".25"/>
-<path d="M15 49 50 62v27L15 75V49Z" fill="currentColor" opacity=".5"/>
-<path d="M85 49 50 62v27l35-14V49Z" fill="currentColor" opacity=".35"/>
-<path d="M36 45 50 14l14 31 3 17-17 9-17-9 3-17Z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="6"/>
-<path d="M36 45 44 30l6-16 6 16 8 15-14 26-14-26Z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="4" opacity=".75"/>
-<path d="M43 47v5M57 47v5" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="5"/>
-<path d="M45 58c3 4 7 4 10 0" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="4"/>
-<path d="M25 75c0-3 3-5 6-4 1-3 6-4 8-1 3-1 6 1 6 5 0 3-3 5-6 5H29c-2 0-4-2-4-5Z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" opacity=".75"/>
+<path d="M16 51 50 39l34 12-34 13-34-13Z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="5"/>
+<path d="M16 51v23l34 15 34-15V51" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="5"/>
+<path d="M50 64v25" fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="5"/>
+<path d="M38 44 50 15l12 29 4 18-16 10-16-10 4-18Z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="5"/>
 `;
 
 export const VAULTBOX_ICON = `
